0c46bffa952f013dcfe574d710b180e5a514a4eb
easti <eastidmyt@gmail.com>
2025-01-15 16:26:32 +0700

    unresolved slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {},
-    screens: {
-      sm: '480px',
-      md: '600px',
-      lg: '600px',
-      xl: '600px',
-    },
+    
   },
   plugins: [],
 }
