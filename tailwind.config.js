860fa09d2581a38f74a75d8c46ce11b37858c4c0
easti <eastidmyt@gmail.com>
2025-01-15 14:59:50 +0700

    unresolve mx

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '600px',
+      lg: '600px',
+      xl: '600px',
+    },
   },
   plugins: [],
 }
